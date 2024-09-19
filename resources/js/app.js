4ee3928c27f15e8362095348c3d5e772ee8033b7
sreerajsree <sreeandsreeraj@gmail.com>
2024-09-19 21:50:39 +0530

    float img left 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,77 @@
 import './bootstrap';
+/* ==================== Show Menu ==================== */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+  
+    toggle.addEventListener("click", () => {
+      //   adding show__menu class
+      nav.classList.toggle("show__menu");
+  
+      //   add show-icon to show and hide the menu icon
+      toggle.classList.toggle("show__icon");
+    });
+  };
+  
+  showMenu("nav-toggle", "nav-menu");
+  
+  /* ==================== Show Dropdown Menu ==================== */
+  const dropdownItems = document.querySelectorAll(".dropdown__item");
+  
+  // 1) Select each dropdown item
+  dropdownItems.forEach((item) => {
+    const dropdownButton = item.querySelector(".dropdown__button");
+  
+    // 2) Select each button click
+    dropdownButton.addEventListener("click", () => {
+      // 7. Select the current show__dropdown class
+      const showDropdown = document.querySelector(".show__dropdown");
+  
+      // 5. Call the toggleItem function
+      toggleItem(item);
+  
+      // 8. Remove the show__dropdown from other items
+      if (showDropdown && showDropdown != item) {
+        toggleItem(showDropdown);
+      }
+    });
+  });
+  
+  // 3) Create a function to display the dropdown
+  const toggleItem = (item) => {
+    // 3.1) Select each dropdown content
+    const dropdownContainer = item.querySelector(".dropdown__container");
+  
+    // 6. If the same item contains the show__dropdown class, remove
+    if (item.classList.contains("show__dropdown")) {
+      dropdownContainer.removeAttribute("style");
+      item.classList.remove("show__dropdown");
+    } else {
+      // 4. Add the maximum height to the dropdown content and add the show__dropdown class
+      dropdownContainer.style.height = dropdownContainer.scrollHeight + "px";
+      item.classList.add("show__dropdown");
+    }
+  };
+  
+  /* ==================== Delete Dropdown Styles ==================== */
+  const mediaQuery = matchMedia("(min-width: 1118px)");
+  const dropdownContainer = document.querySelectorAll(".dropdown__container");
+  
+  // Function to remove dropdown styles in mobile mode or browser resizes
+  const removeStyle = () => {
+    // Validate if the media query reaches 1118px
+    if (mediaQuery.matches) {
+      // Remove the dropdown container height style
+      dropdownContainer.forEach((e) => {
+        e.removeAttribute("style");
+      });
+  
+      // Remove the show__dropdown class from dropdown item
+      dropdownItems.forEach((e) => {
+        e.classList.remove("show__dropdown");
+      });
+    }
+  };
+  
+  addEventListener("resize", removeStyle);
+  
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact', [MainController::class, 'contact'])->name('contact');
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
